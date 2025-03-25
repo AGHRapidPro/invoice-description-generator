36e0aca6e21ed5e86aa6c2d348ec95e2b8fb3099
antoninakus <antonina.kus123@gmail.com>
2025-03-25 08:07:48 +0100

    Adduser panel

--- src/app/user-management/user-management.component.ts
+++ src/app/user-management/user-management.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from "@angular/material/table";
+import { Recipient } from "../models/invoice-model";
+import * as receivers from '../../config/receivers.json';
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
   selector: 'app-user-management',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './user-management.component.html',
   styleUrl: './user-management.component.css'
 })
 export class UserManagementComponent {
+  private receivers: any = receivers;
+  displayedColumns: string[] = ['name', 'lastname', 'index', 'account', 'edit', 'delete'];
+  dataSource: Recipient[] = [];
+
+  constructor() {
+    this.receivers.default.forEach((user: any) => {
+      this.dataSource.push(new Recipient(user));
+    });
+  }
+
+
 
 }
